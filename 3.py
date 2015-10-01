d69a1d59e4ca23f97abc6315fe41bd75af4af25f
ysandeep999 <ysandeep999@gmail.com>
2015-10-01 17:19:39 -0400

    3 prob
    done with dict.
    
    change them to list and try

---
+++ 3.py
@@ -0,0 +1,63 @@
+import string
+
+
+start = 0
+numdict ={}
+
+def pychal(num, line):
+        
+
+    #lngstr1 = input("enter long string : ")
+
+    #lngstr1 = "2)ocr\n3)"
+    
+    lngstr = line.replace("\n","")
+
+    
+
+    lnlen = len(lngstr)
+    global start
+    #start = 0
+
+    for a in lngstr:
+        #print(ord(a))
+        aci = ord(a)
+
+        if aci in range(num,num + 1): #and aci in range(90,127):
+            #print(a)
+            start += 1
+
+        #if(start != 0):
+            #print(start)
+            
+
+    #return start
+    #print("\n\n total len : ",lnlen,"\n output len :",start)
+
+
+def files(nnn):
+
+    global start, numdict
+    f = open('3text.txt', 'r')
+    #num = {}
+
+    for number in range(nnn,nnn + 1):
+        start = 0
+        for line in f:
+            pychal(number, line)
+        numdict[chr(number)] = start
+        #start = 0
+    #print("end")
+    #print(type(num))
+    #print(numdict)
+
+def st():
+    for nn in range(0,127):
+        files(nn)
+
+    for keys,values in numdict.items():
+        if(values > 0 and values < 1000):
+            print(keys,":",values)
+    #print(numdict)
+st()
+#files()
