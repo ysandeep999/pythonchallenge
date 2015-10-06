62b0c0328c92c78b4ce7b7df66004e3ada213fab
ysandeep999 <ysandeep999@gmail.com>
2015-10-06 13:14:32 -0400

    a little improvement
    got more words of that pattern, but sol not found.
    Lot of function written but used only beginhere() patlinefind() and
    pal()

--- 4.py
+++ 4.py
@@ -5,7 +5,7 @@ __author__ = 'prasanth'
 """
 Created on Fri Oct  2 19:14:53 2015
 
-@author: Sam
+@author: San
 """
 #import string
 import re
@@ -14,8 +14,61 @@ patterns = re.compile('[A-Z][A-Z][A-Z][a-z][A-Z][A-Z][A-Z]')
 
 #text = "AAAaAAA"
 
-with open("test.txt") as text:
+with open("4test.txt") as text:
     m = patterns.finditer(text.read())
 
+bsb =[]
+
 for i in m:
-    print(i.group())
+    bsb.append(i.group())
+    #print(i.group())
+print(bsb)
+print(len(bsb))
+
+
+for word in bsb:
+    if(word[0] == word[6] and word[1] == word[5]): # and word[2] == word[6]):
+        print(word)
+
+patlist = []
+
+def patlinefinder(line):        # 
+    patt = re.compile('[A-Z][A-Z][A-Z][a-z][A-Z][A-Z][A-Z]')
+    res = re.search(patt, line)
+    res.groups()
+    if (res):
+        patlist.append(res)
+
+emptlst =[]
+def patlinefind(line):
+    patt = re.compile('[A-Z][A-Z][A-Z][a-z][A-Z][A-Z][A-Z]')
+    linelen = len(line)
+    for l1 in range(0,linelen - 7):
+        if(line[l1].isupper() and line[l1 + 1].isupper() and line[l1 + 2].isupper() and line[l1 + 3].islower() and line[l1 + 4].isupper() and line[l1 + 5].isupper() and line[l1 + 6].isupper()):
+            #emptlst.append(line[l1:l1 + 7])
+            emptlst.append((line[l1:l1 + 7],line[l1:l1+3],line[l1+4:l1+7]))
+            #print(line[l1:l1 + 7])
+            #print(len(emptlst))
+            ##if(line[l1] == line[l1 + 4] and line[l1 + 1] == line[l1 + 5] and line[l1 + 2] == line[l1 + 6]):
+              #  print(line)
+
+
+def pal(lst):
+    for full in lst:
+        if(full[1][0] in full[2] and full[1][2] in full[2]  and full[1][1] in full[2]):
+            print(full)
+
+
+def beginhere():
+    f = open('4test.txt', 'r')
+    #num = {}
+    for line in f:
+        #patlinefinder(line)
+        patlinefind(line)
+
+    #print(patlist[1])
+    print(emptlst)
+    print(len(emptlst))
+    pal(emptlst)
+
+beginhere()
