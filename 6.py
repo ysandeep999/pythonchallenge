c8fc291da2831cb4442f1810a4abb8eff5a1ea68
ysandeep999 <ysandeep999@gmail.com>
2016-01-15 11:27:17 -0500

    test

--- 6.py
+++ 6.py
@@ -1,11 +1,32 @@
-import pprint, pickle
+try:
+    import cPickle as pickle
+except:
+    import pickle
+import pprint
+import sys
 
-pkl_file = open('banner.p', 'rb')
+import pickle, urllib.request
 
-data1 = pickle.load(pkl_file)
-pprint.pprint(data1)
+handle = urllib.request.urlopen("http://www.pythonchallenge.com/pc/def/banner.p")
+data = pickle.load(handle)
+handle.close()
 
-#data2 = pickle.load(pkl_file)
-#pprint.pprint(data2)
+for elt in data:
+    print("".join([e[1] * e[0] for e in elt]))
 
-pkl_file.close()
+# pkl_file = open('banner.p', 'rb')
+#f = open('banner.p', 'rb')
+#data1 = pickle.load(f)
+
+#data1_string = pickle.load(f)
+    #data1_string = f.readlines()
+#data2 = pickle.loads(data1_string)
+#f.close()
+#pprint.pprint(data1_string)
+
+#pprint.pprint(data1)
+
+# data2 = pickle.load(pkl_file)
+# pprint.pprint(data2)
+
+#pkl_file.close()
